d4e5fc0cbef9d54339a5ff7a0828c98f6f9afabd
danielmountford <Dan@Defaults-MacBook-Pro.local>
2017-12-11 11:57:22 +0000

    Mobile menu complete

--- _dev/src/js/header.js
+++ _dev/src/js/header.js
@@ -5,19 +5,22 @@ window.addEventListener(
       docHeight = document.documentElement.offsetHeight;
     var headerFade = document.querySelectorAll(".header-fade");
 
-    window.addEventListener(
-      "scroll",
-      function() {
-        var fix = window.pageYOffset / window.innerHeight * window.innerHeight;
-        var fixValue = fix + "px";
-        var fade = window.pageYOffset / window.innerHeight * 1.5;
-        headerImage.style.top = fixValue;
-        for (var i = 0; i < headerFade.length; i++) {
-          headerFade[i].style.opacity = 1 - fade;
-        }
-      },
-      false
-    );
+    if (window.innerWidth > 960) {
+      window.addEventListener(
+        "scroll",
+        function() {
+          var fix =
+            window.pageYOffset / window.innerHeight * window.innerHeight;
+          var fixValue = fix + "px";
+          var fade = window.pageYOffset / window.innerHeight * 1.5;
+          headerImage.style.top = fixValue;
+          for (var i = 0; i < headerFade.length; i++) {
+            headerFade[i].style.opacity = 1 - fade;
+          }
+        },
+        false
+      );
+    }
   },
   false
 );
--- _dev/src/js/navigation.js
+++ _dev/src/js/navigation.js
@@ -1,14 +1,26 @@
 window.addEventListener("load", function() {
+  // nav appear when scrolled
   var navItems = document.querySelectorAll(".sticky-nav-animation");
-  window.addEventListener("scroll", function() {
-    if (window.pageYOffset > window.innerHeight) {
-      for (var i = 0; i < navItems.length; i++) {
-        navItems[i].classList.add("active");
+  if (window.innerWidth > 960) {
+    window.addEventListener("scroll", function() {
+      if (window.pageYOffset > window.innerHeight) {
+        for (var i = 0; i < navItems.length; i++) {
+          navItems[i].classList.add("active");
+        }
+      } else {
+        for (var i = 0; i < navItems.length; i++) {
+          navItems[i].classList.remove("active");
+        }
       }
-    } else {
-      for (var i = 0; i < navItems.length; i++) {
-        navItems[i].classList.remove("active");
-      }
-    }
+    });
+  }
+
+  // Mobile button animation
+  var mobileButton = document.querySelector(".mobile-menu");
+  var mobileNavigation = document.querySelector(".main-nav-list");
+
+  mobileButton.addEventListener("click", () => {
+    mobileButton.classList.toggle("active");
+    mobileNavigation.classList.toggle("active");
   });
 });
