217e3097e5058f03a1e27f879504fcf635e36a56
danielmountford <Dan@Defaults-MacBook-Pro.local>
2018-01-23 15:47:41 +0000

    Blog post: PageSpeed Insights

--- _dev/src/js/blog.js
+++ _dev/src/js/blog.js
@@ -1,19 +1,21 @@
-var quoteArray = document.querySelectorAll("blockquote a");
+var blockquotes = document.querySelectorAll("blockquote p");
 
-if (quoteArray[0]) {
-  var getPageLink = window.location.href;
-  for (var i = 0; i < quoteArray.length; i++) {
-    var getQuote = quoteArray[i].innerHTML;
-    quoteArray[i].setAttribute(
-      "href",
-      "https://twitter.com/intent/tweet?url=" +
-        getPageLink +
-        "&text=" +
-        getQuote +
-        "&via=AMPstudioUK&",
-      "twitterwindow",
-      "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0"
-    );
+if (blockquotes[0]) {
+  var pageLink = window.location.href;
+  for (var i = 0; i < blockquotes.length; i++) {
+    var blockInner = blockquotes[i].innerHTML;
+    var quote = blockquotes[i].querySelector("a").innerHTML;
+
+    blockquotes[i].innerHTML =
+      '<a class="quote-wrapper" target="_blank" href="https://twitter.com/intent/tweet?url=' +
+      pageLink +
+      "&text=" +
+      quote +
+      "&via=AMPstudioUK&" +
+      "twitterwindow" +
+      "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0" +
+      '"></a>' +
+      blockInner;
   }
 }
 
