82037905d2229220f67e05d5f881b1ff4d3f9c7b
danielmountford <dan@amp.studio>
2018-02-05 17:56:00 +0000

    Landing page complete

--- _dev/src/js/landing-page.js
+++ _dev/src/js/landing-page.js
@@ -1,4 +1,17 @@
 const form = document.querySelector("form");
+const animate = document.querySelectorAll(".animate");
+
+// Fields
+const nameField = document.querySelector("#name");
+const emailField = document.querySelector("#email");
+const messageField = document.querySelector("#message");
+
+// Labels
+const nameLabel = document.querySelector("#name-label");
+const emailLabel = document.querySelector("#email-label");
+const messageLabel = document.querySelector("#message-label");
+
+const responseField = document.querySelector("#response");
 
 form.addEventListener("submit", e => {
   e.preventDefault();
@@ -6,23 +19,69 @@ form.addEventListener("submit", e => {
   const email = e.target[1].value;
   const company = e.target[2].value;
   const industry = e.target[3].value;
-  const details = e.target[4].value;
+  const message = e.target[4].value;
 
   const data = {
     name,
     email,
     company,
     industry,
-    details
+    message
   };
 
   axios
-    .post("https://api.amp.studio/contact/websites-from-795", data)
+    .post("http://localhost:9001/contact/landing-page", data)
     .then(response => {
-      if (response.status === 200) {
-        success();
+      const errors = response.data;
+
+      if (errors.name) {
+        handleError(nameLabel, nameField, "Please enter your name:", false);
       } else {
-        error();
+        handleError(nameLabel, nameField, "Your name:", true);
       }
+
+      if (errors.email) {
+        handleError(emailLabel, emailField, "Please enter your email:", false);
+      } else {
+        handleError(emailLabel, emailField, "Your email:", true);
+      }
+
+      if (errors.message) {
+        handleError(
+          messageLabel,
+          messageField,
+          "Please enter your message:",
+          false
+        );
+      } else {
+        handleError(
+          messageLabel,
+          messageField,
+          "Describe your new website:",
+          true
+        );
+      }
+
+      if (!errors) {
+        for (var i = 0; i < animate.length; i++) {
+          animate[i].classList.add("success");
+        }
+      }
+    })
+
+    .catch(error => {
+      responseField.innerHTML =
+        "<img src='/assets/img/error-icon.svg' /> <p class='error'>Error, please try again.</p>";
     });
 });
+
+function handleError(label, input, text, success) {
+  label.innerHTML = text;
+  if (success) {
+    label.classList.remove("error");
+    input.classList.remove("error");
+  } else {
+    label.classList.add("error");
+    input.classList.add("error");
+  }
+}
